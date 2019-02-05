f4f35e8f1e7197cbb94999be99f5ca09098bcb19
Austin Moran <40526277+Kata7@users.noreply.github.com>
2019-02-05 14:56:16 -0700

    Changed columns to text (unlimited length) from string

--- dadsbackend/migrations/20190205143000_jokesMigrate.js
+++ dadsbackend/migrations/20190205143000_jokesMigrate.js
@@ -1,8 +1,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('jokes', (table) => {
     table.increments('id');
-    table.string('text').notNullable().defaultsTo('');
-    table.string('url').notNullable().defaultsTo('');
+    table.text('text').notNullable().defaultsTo('');
+    table.text('url').notNullable().defaultsTo('');
   })
   
 };
